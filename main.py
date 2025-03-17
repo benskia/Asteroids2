998fb2cb14809572c75fc03f12845d75c2acbc1c
Abraham Benski <AbrahamBenski@proton.me>
2025-03-17 14:44:17 -0500

    add circles and player

--- main.py
+++ main.py
@@ -7,19 +7,32 @@ from constants import (
     ASTEROID_SPAWN_RATE,
     ASTEROID_MAX_RADIUS
 )
+from player import Player
 
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt: int = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     # Main Game Loop
     while True:
+        pygame.event.pump()
         screen.fill((0, 0, 0))
+
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
